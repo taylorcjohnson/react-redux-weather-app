6e9abcfe2e6dfe46c4024d51c0ab71e64b6591ab
Taylor Johnson <taylor.c.johnson@usace.army.mil>
2016-09-29 17:45:02 -0400

    Finish axios get request

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,9 +7,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
 
   return {
     type: FETCH_WEATHER,
-    payload:
+    payload: request
   };
 }
