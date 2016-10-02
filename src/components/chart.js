fe8bfa8e02de4a8f145bd3d462112cd0563b4f3d
Taylor Johnson <taylor.c.johnson@usace.army.mil>
2016-10-02 14:08:47 -0400

    Update chart component

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import { Sparklines, SparklinesCurve, SparklinesReferenceLine } from 'react-sparklines';
 import _ from 'lodash';
 
 function average(data) {
@@ -10,7 +10,7 @@ export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={180} data={props.data}>
-        <SparklinesLine color={props.color} />
+        <SparklinesCurve color={props.color} />
         <SparklinesReferenceLine type="avg" />
       </Sparklines>
       <div>{average(props.data)} {props.units}</div>
