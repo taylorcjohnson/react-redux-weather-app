f5de276d8de3216f02665b757e66e539959cd6d7
Taylor Johnson <taylor.c.johnson@usace.army.mil>
2016-10-02 12:55:35 -0400

    Add react-google-maps to show city location

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,10 +9,11 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord; // ES6 syntax - pulling two properties off object at the same time
 
     return (
       <tr key={name}>
-        <td className="col-md-3">{name}</td>
+        <td className="col-md-3"><GoogleMap lon={lon} lat={lat}/></td>
         <td className="col-md-3"><Chart data={temps} color="orange" units="K" /></td>
         <td className="col-md-3"><Chart data={pressures} color="green" units="hPa"/></td>
         <td className="col-md-3"><Chart data={humidities} color="blue" units="%"/></td>
